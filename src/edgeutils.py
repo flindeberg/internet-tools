076da9894b369ff183509ab307555d6069919272
flindeberg <flindeberg@gmail.com>
2020-04-08 16:40:27 +0200

    Added edge classes

--- src/edgeutils.py
+++ src/edgeutils.py
@@ -1,6 +1,30 @@
 from enum import Enum
 from dataclasses import dataclass
 
+class TraceType(Enum):
+    full = 0 # a full and complete trace
+    fullbutend = 1 # all missing hosts are at the end
+    fullbutmid = 2 # all missing hosts are in the middle
+    missing = 3 # some other form
+
+    @classmethod
+    def getTraceStatus(cls, trace: list):
+        ## Set the tracing flags
+        if "*" not in trace:
+            # No stars, it is a full trace
+            return TraceType.full
+        elif trace[-1] != "*":
+            # We have the end, so we are missing mid
+            return TraceType.fullbutmid
+        else:
+            # lets check if they are all at the end
+            # shallow copy to be on the safe side (remove later?)
+            rev = trace.copy()
+            while rev[-1] == "*":
+                rev = rev[:-1]
+            # now we have removed the end *:s, lets see if we still have any
+            return TraceType.fullbutend if "*" not in rev else TraceType.missing
+
 class EdgeType(Enum):
     start = 0
     ihost = 1
