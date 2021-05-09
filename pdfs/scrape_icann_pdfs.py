ad8e565ac180e94aabb3065a59c2e7790edbe396
flindeberg <flindeberg@gmail.com>
2021-05-09 15:19:23 +0200

    Linted pdfscraper

--- pdfs/scrape_icann_pdfs.py
+++ pdfs/scrape_icann_pdfs.py
@@ -9,32 +9,44 @@ from scrapy.dupefilters import RFPDupeFilter
 import os
 from urllib.parse import urlparse
 
+
 class MyDupeFilter(RFPDupeFilter):
     ## class for catching pdf dupes due to auto-generated links
 
-    def request_fingerprint(self, req : Request) -> str:
+    def request_fingerprint(self, req: Request) -> str:
         if req.url.endswith("application/pdf"):
             # get the part of the url which seems to identify the file itself
-            pdfname = str.join('-', req.url.split('/')[-2].split('-')[0:-3]) + ".pdf"
+            pdfname = str.join("-", req.url.split("/")[-2].split("-")[0:-3]) + ".pdf"
             print("Caught pdf-export called {:}".format(pdfname))
             return "icann.org/mypdfs/{:}".format(pdfname)
         else:
             return super().request_fingerprint(req)
 
+
 class IcannSpider(CrawlSpider):
 
     custom_settings = {
-        'DUPEFILTER_CLASS': 'scrape_icann_pdfs.MyDupeFilter',
+        "DUPEFILTER_CLASS": "scrape_icann_pdfs.MyDupeFilter",
     }
 
     # .remove
-    name = 'icann'
+    name = "icann"
     allowed_domains = ["icann.org"]
-    start_urls = ['https://www.icann.org/resources/pages/cctlds/cctlds-en', 'https://icann.org']
-    rules = [Rule(LinkExtractor(
-                    deny_extensions=(ele for ele in linkextractors.IGNORED_EXTENSIONS if ele != "pdf"),
-                    ), 
-                callback='parse_item', follow=True)]
+    start_urls = [
+        "https://www.icann.org/resources/pages/cctlds/cctlds-en",
+        "https://icann.org",
+    ]
+    rules = [
+        Rule(
+            LinkExtractor(
+                deny_extensions=(
+                    ele for ele in linkextractors.IGNORED_EXTENSIONS if ele != "pdf"
+                ),
+            ),
+            callback="parse_item",
+            follow=True,
+        )
+    ]
 
     ## Parse already downloaded urls
     def parse_item(self, response):
